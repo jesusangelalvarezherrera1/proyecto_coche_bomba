02aa20f8b260b4a693447c6940cc5e25b4d7011c
jesusangelalvarezherrera1 <jesusangelalvarezherrera10@gmail.com>
2023-10-11 12:25:00 -0600

    se agrego el menu

--- server.py
+++ server.py
@@ -8,19 +8,23 @@ port = 3000
 
 class miServer(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path=="/":
+        if self.path == "/":
             self.path = "index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
         
-        if self.path=="/frmclientes":
+        if self.path == "/frmclientes":
             self.path = "clientes.html"
             return SimpleHTTPRequestHandler.do_GET(self)
         
-        if self.path=="/frmbusqueda_clientes":
+        if self.path == "/frmbusqueda_clientes":
             self.path = "busqueda_clientes.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+        if self.path == "/frmmenu":
+            self.path = "menu.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
        
-        if self.path=="/clientes":
+        if self.path == "/clientes":
             self.send_response(200)
             self.end_headers()
             self.wfile.write(json.dumps(crud_clientes.consultar_clientes()).encode('utf-8'))
@@ -31,15 +35,17 @@ class miServer(SimpleHTTPRequestHandler):
         datos = datos.decode()
         datos = parse.unquote(datos)
         datos = json.loads(datos)
-        if self.path=="/clientes":
+        
+        if self.path == "/clientes":
             resp = {"msg": crud_clientes.administrar(datos)}
-        if self.path=="/buscar_clientes":
+        if self.path == "/buscar_clientes":
             resp = crud_clientes.consultar_clientes(datos)
         
         self.send_response(200)
         self.end_headers()
         self.wfile.write(json.dumps(resp).encode())
+
         
-print("Ejecuntando server en puerto ", port)
+print("Ejecutando server en puerto ", port)
 server = HTTPServer(("localhost", port), miServer)
 server.serve_forever()
